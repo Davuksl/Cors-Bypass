efeb701d22738d2ef4ce7b86387ec304ed2b4297
Davuks <skibidilol553@gmail.com>
2025-06-22 07:14:01 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,6 @@
 var host = process.env.HOST || '0.0.0.0';
 // Listen on a specific port via the PORT environment variable
 var port = process.env.PORT || 8080;
-
-// Grab the blacklist from the command-line so that we can update the blacklist without deploying
-// again. CORS Anywhere is open by design, and this blacklist is not used, except for countering
-// immediate abuse (e.g. denial of service). If you want to block all origins except for some,
-// use originWhitelist instead.
-var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
-var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
 function parseEnvList(env) {
   if (!env) {
     return [];
@@ -16,14 +9,10 @@ function parseEnvList(env) {
   return env.split(',');
 }
 
-// Rate limit отключён:
-// var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELIMIT);
-
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
     originWhitelist: [],
     requireHeader: [],
-  // checkRateLimit: checkRateLimit, // отключено
   removeHeaders: [
     'cookie',
     'cookie2',
@@ -40,7 +29,6 @@ cors_proxy.createServer({
   ],
   redirectSameOrigin: true,
   httpProxyOptions: {
-    // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
   },
 }).listen(port, host, function() {
